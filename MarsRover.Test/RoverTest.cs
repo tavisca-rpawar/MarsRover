fff53ab725612ff21f6040138daed8fc8ea1ddaf
tavisca-rpawar <51902720+tavisca-rpawar@users.noreply.github.com>
2019-12-09 16:38:22 +0530

    Update RoverTest.cs
    Resolved PR review changes and added more test cases

--- MarsRover.Test/RoverTest.cs
+++ MarsRover.Test/RoverTest.cs
@@ -5,26 +5,35 @@ namespace MarsRover.Test
 {
     public class RoverTest
     {
-        
         [Theory]
         [InlineData("(0,1,e)", "MML", "(2,1,N)")]
         [InlineData("(0,0,N)", "MmM", "(0,3,N)")]
         [InlineData("(10,5,n)", "MMLMrmML", "(9,9,W)")]
-        public void rover_test(string position, string moves, string expected)
+        public void Rover_Test_Should_Return_Final_Position(string position, string moves, string expected)
         {
             RoverPositionAllocator rover = new RoverPositionAllocator();
             rover.InitialPosition(position);
             string actual = rover.Moves(moves);
             actual.Equals(expected);
         }
-        [Theory]
-        [InlineData("mkl", "Invalid Input")]
-        [InlineData("mmmOOl", "Invalid Input")]
-        public void rover_moves_validator_test(string moves, string expected)
+
+        [Fact]
+        public void Validate_InitialPositin_Test_Should_Return_False_For_Inavalid_InitialPosition()
         {
-            RoverPositionAllocator rover = new RoverPositionAllocator();
-            string actual = rover.Moves(moves);
+            string[] initialPosition = { "1", "2", "3" };
+            bool actual = Validators.ValidateIniilaPosition(initialPosition);
+            bool expected = false;
+            actual.Equals(expected);
+        }
+        [Theory]
+        [InlineData("MMLR", true)]
+        [InlineData("MnM", false)]
+        [InlineData("MMLMrmkldL", false)]
+        public void Validator_Moves_Test_Should_Return_False_For_Inavalid_Rover_Moves(string moves, bool expected)
+        {
+            bool actual = Validators.ValidateMoves(moves);
             actual.Equals(expected);
         }
     }
 }
+
